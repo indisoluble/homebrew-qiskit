df93dea6135113d05e8334e2043d8ed46d25c039
Enrique de la Torre <indisoluble_dev@me.com>
2018-06-09 21:39:26 +0200

    Add Homebrew formula: qasm_simulator_cpp

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # homebrew-qiskit
 Homebrew Tap for QISKit
+
+## Installation
+
+```
+brew tap indisoluble/qiskit
+```
+
+### QASM simulator without full OpenMP parallelization
+
+```
+brew install qasm_simulator_cpp
+```
+
+### QASM simulator with OpenMP
+**NOTICE: Requires GCC. It will be automatically installed if necessary** 
+
+```
+brew install qasm_simulator_cpp --with-openmp
+```
+
+## Running
+
+```
+qasm_simulator_cpp input.json
+```
+
+Or:
+
+```
+cat input.json | qasm_simulator_cpp -
+```
---
+++ qasm_simulator_cpp.rb
@@ -0,0 +1,87 @@
+class QasmSimulatorCpp < Formula
+  desc "C++ quantum circuit simulator with realistic noise"
+  homepage "https://qiskit.org/"
+  url "https://github.com/QISKit/qiskit-core/archive/0.5.3.tar.gz"
+  sha256 "48798688c409742e6259ee721efabfb60ffe55c84c0557885dcd9d7d67602827"
+
+  option "with-openmp", "Enable OpenMP multithreading"
+
+  depends_on "cmake" => :build
+  depends_on "gcc" if build.with? "openmp"
+
+  fails_with :clang if build.with? "openmp"
+
+  # ONLY build qasm simulator
+  patch :DATA
+
+  def install
+    if build.with? "gcc"
+      args = ["-DCMAKE_CXX_COMPILER=g++-7"]
+    else
+      args = ["-DSTATIC_LINKING=False"]
+    end
+
+    system "cmake", ".", *std_cmake_args, *args
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"input.json").write <<~EOS
+      {
+          "id": "test_qobj",
+          "config": {
+              "shots": 4,
+              "seed": 0,
+              "initial_state": [1, 0, 0, 1]
+          },
+          "circuits": []
+      }
+    EOS
+
+    system "#{bin}/qasm_simulator_cpp", "input.json"
+  end
+end
+
+__END__
+--- a/CMakeLists.txt
++++ b/CMakeLists.txt
+@@ -7,25 +7,8 @@ set(QISKIT_VERSION ${PROJECT_VERSION} CACHE INTERNAL "VERSION OF QISKIT")
+
+ include(cmake/defaults.cmake)
+
+-if (ENABLE_TARGETS_NON_PYTHON)
+-    # Add Toolchain utilities for C++ sources
+-    include(cmake/toolchain-utils.cmake)
++# Add Toolchain utilities for C++ sources
++include(cmake/toolchain-utils.cmake)
+
+-    # Add C++ Qiskit Simulator
+-    add_subdirectory(${PROJECT_SOURCE_DIR}/src/qasm-simulator-cpp)
+-
+-    # Add Python distributable package generator
+-    include(cmake/python-build.cmake)
+-    add_pypi_package_target(pypi_package both)
+-endif()
+-
+-include(cmake/tests.cmake)
+-
+-if (ENABLE_TARGETS_QA)
+-    # Add QA tools: lint, style and sphinx
+-    include(cmake/code-qa.cmake)
+-    add_lint_target()
+-    add_code_style_target()
+-    add_doc_target(html "./" "_build/")
+-    add_coverage_target()
+-endif()
++# Add C++ Qiskit Simulator
++add_subdirectory(${PROJECT_SOURCE_DIR}/src/qasm-simulator-cpp)
+--- a/src/qasm-simulator-cpp/CMakeLists.txt
++++ b/src/qasm-simulator-cpp/CMakeLists.txt
+@@ -152,3 +152,7 @@ endif()
+ # Tests
+ # TODO: Enable them when ready
+ #add_subdirectory(${QASM_SIMULATOR_CPP_DIR}/test)
++
++install(TARGETS qasm_simulator_cpp
++       RUNTIME DESTINATION bin
++)
